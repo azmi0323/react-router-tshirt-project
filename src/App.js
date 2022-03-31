5f31c6a0c2abdc4d73e368a0392ee7404a750a5f
AzmiHabib <azmihabib0323@gmail.com>
2022-04-01 03:15:36 +0600

    commit after set all components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 import OrderReview from './components/OrderReview/OrderReview';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
 
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <OrderReview></OrderReview>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/orderReview' element={<OrderReview></OrderReview>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
