a98b6cb5a10ed5550a334768278e1b008c453044
AzmiHabib <azmihabib0323@gmail.com>
2022-04-01 14:11:51 +0600

    fetch data and show on ui

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
+import useTShirts from "../../hooks/useTShirts";
+import Cart from "../Cart/Cart";
+import TShirt from "../TShirt/TShirt";
 import "./Home.css";
 const Home = () => {
+  const [tShirts,setTShirts] = useTShirts();
   return (
-  <div>
-      <h3>this is home</h3>
+  <div className="home-container">
+      <div className="tshirt-container">
+        {
+          tShirts.map(tShirt=><TShirt tShirt={tShirt} key={tShirt._id}></TShirt>)
+        }
+      </div>
+      <div className="cart-container">
+        <Cart></Cart>
+      </div>
   </div>
   );
 };
