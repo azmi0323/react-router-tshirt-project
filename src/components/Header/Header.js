48b3003d502ff76be8777c97b8f57e1ce886410a
AzmiHabib <azmihabib0323@gmail.com>
2022-04-01 12:55:27 +0600

    header part complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 const Header = () => {
-    return (
-        <div>
-            <h3>this is header</h3>
-        </div>
-    );
+  return (
+    <div className="header-part">
+      <div>
+        <h2>TShirt Mania</h2>
+      </div>
+      <div >
+        <Link className="header-link" to="/">Home</Link>
+        <Link className="header-link" to="/orderReview">OrderReview</Link>
+        <Link className="header-link" to="/about">About</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
