a8586d84ae558ed6418785fc35e74cfce6bb68ca
AzmiHabib <azmihabib0323@gmail.com>
2022-04-01 15:38:54 +0600

    item can selected

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Cart.css'
-const Cart = () => {
+const Cart = ({cart}) => {
     return (
         <div>
             <h3>This is Cart</h3>
+            {
+                cart.map(tShirt=><p>{tShirt.name}</p>)
+            }
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import useTShirts from "../../hooks/useTShirts";
 import Cart from "../Cart/Cart";
 import TShirt from "../TShirt/TShirt";
 import "./Home.css";
 const Home = () => {
-  const [tShirts,setTShirts] = useTShirts();
+  const [tShirts, setTShirts] = useTShirts();
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (selectedItem) => {
+    const newCart = [...cart, selectedItem];
+    setCart(newCart);
+  };
   return (
-  <div className="home-container">
+    <div className="home-container">
       <div className="tshirt-container">
-        {
-          tShirts.map(tShirt=><TShirt tShirt={tShirt} key={tShirt._id}></TShirt>)
-        }
+        {tShirts.map((tShirt) => (
+          <TShirt
+            handleAddToCart={handleAddToCart}
+            tShirt={tShirt}
+            key={tShirt._id}
+          ></TShirt>
+        ))}
       </div>
       <div className="cart-container">
-        <Cart></Cart>
+        <Cart cart={cart}></Cart>
       </div>
-  </div>
+    </div>
   );
 };
 
